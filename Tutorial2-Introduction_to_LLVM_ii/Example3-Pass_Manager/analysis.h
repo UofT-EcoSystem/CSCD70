409fee658adb0933e11a6f005ba9937e3554fb92
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-01-18 14:34:51 -0500

    Added the example on LLVM Pass Manager.

---
+++ Tutorial2-Introduction_to_LLVM_ii/Example3-Pass_Manager/analysis.h
@@ -0,0 +1,24 @@
+#include <llvm/Pass.h>
+
+using namespace llvm;
+
+class Analysis : public ModulePass
+{
+private:
+	std::vector < unsigned > _my_stats;
+public:
+	static char ID;
+
+	Analysis() : ModulePass(ID)
+	{}
+
+	~Analysis()
+	{}
+
+	virtual void getAnalysisUsage(AnalysisUsage & AU) const;
+  
+	virtual bool runOnModule(Module & M);
+
+	// Return the gathered statstics.
+	std::vector < unsigned > getStats() const { return _my_stats; }
+};
