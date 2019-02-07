f6cf7d6b3520fa61c8742a26eab83965e1b9d4e0
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-02-06 22:53:47 -0500

    Added the starter code for liveness.

--- Assignment2-Dataflow_Analysis/src/liveness.cpp
+++ Assignment2-Dataflow_Analysis/src/liveness.cpp
@@ -1,1 +1,28 @@
 #include "dfa/framework.h"
+
+// @TODO 
+
+namespace {
+class Liveness : public FunctionPass // @TODO Change the base class if necessary.
+{
+public:
+	static char ID;
+
+	Liveness() : FunctionPass(ID) {}
+
+	// @TODO Add or remove method definitions if necessary.
+	
+	virtual void getAnalysisUsage(AnalysisUsage & AU) const
+	{
+		AU.setPreservesAll();
+	}
+
+	bool runOnFunction(Function & func)
+	{
+		return false;
+	}
+};
+
+char Liveness::ID = 1; 
+RegisterPass < Liveness > Y ("liveness", "Liveness");
+} // namespace anonymous
