c51bed60001a67dad20a5f7b8c6065ad9d60c23f
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-02-01 15:57:12 -0500

    Reformatted the test configuration files.

--- experimental/llvm-9/tests/lit.site.cfg.in.py
+++ experimental/llvm-9/tests/lit.site.cfg.in.py
@@ -1,9 +1,11 @@
 import lit.llvm
 
-config.llvm_tools_dir = "@LLVM_CONFIG_BINDIR@"
-config.llvm_dylib_ext = "@CMAKE_SHARED_LIBRARY_SUFFIX@"
-config.llvm_dylib_dir = "@CMAKE_LIBRARY_OUTPUT_DIR@"
-config.test_exec_root = "@CMAKE_CURRENT_BINARY_DIR@"
+
+config.test_source_root = "@CMAKE_CURRENT_SOURCE_DIR@"
+config.test_exec_root   = "@CMAKE_CURRENT_BINARY_DIR@"
+config.llvm_config_bindir = "@LLVM_CONFIG_BINDIR@"
+config.substitutions.append(('%dylibext', "@CMAKE_SHARED_LIBRARY_SUFFIX@"))
+config.substitutions.append(('%dylibdir', "@CMAKE_LIBRARY_OUTPUT_DIR@"))
 
 lit.llvm.initialize(lit_config, config)
 lit_config.load_config(config, "@CMAKE_CURRENT_SOURCE_DIR@/lit.cfg.py")
