bec5fa385f5c878134ec993f18371b3b1e03f8f1
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-12-19 21:55:53 -0500

    Working on clang-format.

---
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/FunctionInfo.cpp
@@ -0,0 +1,37 @@
+
+#include <llvm/Pass.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace llvm;
+
+
+namespace {
+
+class FunctionInfo final : public ModulePass
+{
+ public:
+  static char ID;
+
+	FunctionInfo() : ModulePass(ID) {}
+	virtual ~FunctionInfo() override {}
+  
+  // We don't modify the program, so we preserve all analysis.
+	virtual void getAnalysisUsage(AnalysisUsage & AU) const override {
+		AU.setPreservesAll();
+	}
+	virtual bool runOnModule(Module & M) override {
+	outs() << "CSCD70 Functions Information Pass" << "\n";
+
+		// @TODO Please complete this function.
+		
+		return false;
+	}
+};
+
+char FunctionInfo::ID = 0;
+RegisterPass < FunctionInfo > X (
+	  "function-info",
+	  "CSCD70: Functions Information");
+
+}  // namespace anonymous
