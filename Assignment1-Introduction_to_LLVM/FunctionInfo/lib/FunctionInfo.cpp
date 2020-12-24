e247e11f826e345231672fa262be93ba61204e09
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-12-23 22:49:50 -0500

    Reformatted the code.

--- Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
@@ -1,5 +1,5 @@
-#include <llvm/Pass.h>
 #include <llvm/IR/Module.h>
+#include <llvm/Pass.h>
 #include <llvm/Support/raw_ostream.h>
 
 using namespace llvm;
@@ -7,30 +7,31 @@ using namespace llvm;
 
 namespace {
 
-class FunctionInfo final : public ModulePass
-{
+class FunctionInfo final : public ModulePass {
  public:
   static char ID;
 
   FunctionInfo() : ModulePass(ID) {}
   virtual ~FunctionInfo() override {}
-  
+
   // We don't modify the program, so we preserve all analysis.
-  virtual void getAnalysisUsage(AnalysisUsage & AU) const override {
+  virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
     AU.setPreservesAll();
   }
-  virtual bool runOnModule(Module & M) override {
-    outs() << "CSCD70 Functions Information Pass" << "\n";
+
+  virtual bool runOnModule(Module &M) override {
+    outs() << "CSCD70 Functions Information Pass"
+           << "\n";
 
     // @TODO Please complete this function.
-    
+
     return false;
   }
-};
+};  // class FunctionInfo
 
 char FunctionInfo::ID = 0;
-RegisterPass < FunctionInfo > X (
+RegisterPass<FunctionInfo> X(
     "function-info",
     "CSCD70: Functions Information");
 
-}  // namespace anonymous
+}  // anonymous namespace
