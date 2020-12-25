2a482028ed91cc5f437f3f578b8cb2548dac54d5
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2020-12-25 17:39:16 -0500

    Merged lit configurations into a single file.

---
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/tests/lit.cfg.in.py
@@ -0,0 +1,24 @@
+import lit
+import lit.llvm
+
+lit.llvm.initialize(lit_config, config)
+
+from lit.llvm import llvm_config
+
+
+config.name = 'FunctionInfo'
+config.test_format = lit.formats.ShTest()
+                     # The ShTest files contain some number of shell-like
+                     # command pipelines, along with assertions about what
+                     # should be in the output.
+config.test_source_root = "@CMAKE_CURRENT_SOURCE_DIR@"
+config.test_exec_root   = "@CMAKE_CURRENT_BINARY_DIR@"
+config.suffixes = ['.c', '.ll']
+config.substitutions.append((r'%dylibext', "@CMAKE_SHARED_LIBRARY_SUFFIX@"))
+config.substitutions.append((r'%dylibdir', "@CMAKE_LIBRARY_OUTPUT_DIR@"))
+
+# Extra config attribute for tool substitutions.
+config.llvm_config_bindir = "@LLVM_CONFIG_BINDIR@"
+llvm_config.add_tool_substitutions(
+        ["clang", "opt", "FileCheck"],
+        config.llvm_config_bindir)
