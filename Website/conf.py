a82174ff8f63fb742f44415a844acbf696b81ef3
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-01-05 22:24:47 -0500

    Changed the sidebar.

--- Website/conf.py
+++ Website/conf.py
@@ -53,3 +53,10 @@ html_theme = 'sphinxdoc'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_sidebars = {
+    '**': [
+        'indexsidebar.html',
+        'searchbox.html',
+    ]
+}
