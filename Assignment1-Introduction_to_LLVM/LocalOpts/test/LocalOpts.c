196f61dde992823bda44e6abe5f938dda5325f9a
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-01-09 16:55:48 -0500

    Checkpoint before switching to assembly test case.

--- Assignment1-Introduction_to_LLVM/LocalOpts/test/LocalOpts.c
+++ Assignment1-Introduction_to_LLVM/LocalOpts/test/LocalOpts.c
@@ -1,11 +1,12 @@
 // RUN: clang -O0 -Xclang -disable-O0-optnone -emit-llvm -c %s -o %basename_t.bc
-// RUN: 
 // RUN: llvm-dis %basename_t.bc -o=%basename_t.ll
+// RUN: opt -mem2reg %basename_t.bc -o %basename_t.m2r.bc
 // RUN: opt -load %dylibdir/libLocalOpts%dylibext \
 // RUN:     -algebraic-identity -strength-reduction -multi-inst-opt \
-// RUN:     %basename_t.bc | \
-// RUN: FileCheck --match-full-lines %s
-
+// RUN:     %basename_t.m2r.bc | llvm-dis - -o=%basename_t.opt.ll
+// RUN: FileCheck --match-full-lines %s --input-file=%basename_t.ll
+/*! @todo(cscd70) Add the CHECK directives similar to those in Loop.c.
+ */
 int LocalOpts(int a) {
   int r0 = a + 0;
   int r1 = r0 * 16;
