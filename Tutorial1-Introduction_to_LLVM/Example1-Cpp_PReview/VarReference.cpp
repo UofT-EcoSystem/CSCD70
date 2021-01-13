e5b84976d942d6fb065b9a5b97cc8f283dafea97
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-01-13 16:42:13 -0500

    Fixed the print statements for consistency.

--- Tutorial1-Introduction_to_LLVM/Example1-Cpp_PReview/VarReference.cpp
+++ Tutorial1-Introduction_to_LLVM/Example1-Cpp_PReview/VarReference.cpp
@@ -14,9 +14,9 @@ int main() {
            //` x_ref` and the value that `x_ptr` is pointing to and at the same
            // time, but NOT the value of `x_copy`.
   std::cout << "x: " << x << ", "
-            << "ptr_x: " << *x_ptr << ", "
-            << "ref_x: " <<  x_ref << ", "
-            << "copy_x: " << x_copy << std::endl;  // `endl` is the end-line.
+            << "x_ptr: " << *x_ptr << ", "
+            << "x_ref: " <<  x_ref << ", "
+            << "x_copy: " << x_copy << std::endl;  // `endl` is the end-line.
   /**
    * @note We do NOT need to "take the address of" and/or "dereference" when
    *       dealing when references, which make them a cleaner solution compared
