70d5488e6d81b585272ca8fd667bd5d572f54cfb
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-02-25 17:18:19 -0500

    Added opt to the tool substitution list.

--- Assignment1-Introduction_to_LLVM/FunctionInfo/test/lit.cfg.in.py
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/test/lit.cfg.in.py
@@ -27,5 +27,5 @@ config.substitutions.append((r'%dylibdir', "@CMAKE_LIBRARY_OUTPUT_DIR@"))
 # FileCheck is not in the default executable path, therefore we need to add it manually.
 config.llvm_config_bindir = "@LLVM_BINDIR@"
 llvm_config.add_tool_substitutions(
-        ["FileCheck"],
+        ["opt", "FileCheck"],
         config.llvm_config_bindir)
