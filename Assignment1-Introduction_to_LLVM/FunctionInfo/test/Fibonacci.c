1862151c428228656543519463bd200c2e7c949a
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2023-01-07 22:32:52 -0500

    Migrate the Fibonacci test case

---
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/test/Fibonacci.c
@@ -0,0 +1,34 @@
+// clang-format off
+// RUN: clang -O2 -S -emit-llvm -c %s -o %basename_t.ll
+// RUN: opt -load-pass-plugin=%dylibdir/libFunctionInfo.so -passes=function-info -disable-output 2>&1 %basename_t.ll | \
+// RUN: FileCheck --match-full-lines --check-prefix=SAMPLE %s
+// clang-format on
+/**
+ * @todo(cscd70) Please Remove the `--check-prefix=SAMPLE` option and add the
+ *               CHECK directives similar to those in Loop.c.
+ */
+// SAMPLE: CSCD70 Function Information Pass
+#include <stdio.h>
+
+int printf(const char *format, ...) {
+  int ret;
+  va_list args;
+  va_start(args, format);
+  ret = vfprintf(stdout, format, args);
+  va_end(args);
+
+  return ret;
+}
+
+int Fibonacci(const int n) {
+  if (n == 0) {
+    printf("f(0) = 0");
+    return 0;
+  }
+  if (n == 1) {
+    printf("f(1) = 1");
+    return 1;
+  }
+  printf("f(%d) = f(%d) + f(%d)", n, n - 1, n - 2);
+  return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
