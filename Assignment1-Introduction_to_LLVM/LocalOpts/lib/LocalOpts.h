3ba883f464167bf9f623b17140081a73e5245aee
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2023-01-20 15:06:20 -0500

    Include headers in clang-tidy

--- Assignment1-Introduction_to_LLVM/LocalOpts/lib/LocalOpts.h
+++ Assignment1-Introduction_to_LLVM/LocalOpts/lib/LocalOpts.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once // NOLINT(llvm-header-guard)
 
 #include <llvm/Pass.h>
 #include <llvm/Passes/PassBuilder.h>
@@ -8,19 +8,19 @@
 class AlgebraicIdentityPass final
     : public llvm::PassInfoMixin<AlgebraicIdentityPass> {
 public:
-  llvm::PreservedAnalyses run(llvm::Function &M,
+  llvm::PreservedAnalyses run(llvm::Function &,
                               llvm::FunctionAnalysisManager &);
 }; // class AlgebraicIdentityPass
 
 class StrengthReductionPass final
     : public llvm::PassInfoMixin<StrengthReductionPass> {
 public:
-  llvm::PreservedAnalyses run(llvm::Function &M,
+  llvm::PreservedAnalyses run(llvm::Function &,
                               llvm::FunctionAnalysisManager &);
 }; // class StrengthReductionPass
 
 class MultiInstOptPass final : public llvm::PassInfoMixin<MultiInstOptPass> {
 public:
-  llvm::PreservedAnalyses run(llvm::Function &M,
+  llvm::PreservedAnalyses run(llvm::Function &,
                               llvm::FunctionAnalysisManager &);
 }; // class MultiInstOptPass
