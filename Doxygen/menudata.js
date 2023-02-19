5a850482d53b093152c815f6221228b9ef99ebf0
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2023-02-19 22:33:52 +0000

    Deploying to gh-pages from @ UofT-EcoSystem/CSCD70@4c82f552d8c5c9e5b34238c6e295f786bd5c76aa 🚀

--- Doxygen/menudata.js
+++ Doxygen/menudata.js
@@ -45,7 +45,6 @@ var menudata={children:[
 {text:"f",url:"functions.html#index_f"},
 {text:"g",url:"functions.html#index_g"},
 {text:"i",url:"functions.html#index_i"},
-{text:"k",url:"functions.html#index_k"},
 {text:"l",url:"functions.html#index_l"},
 {text:"m",url:"functions.html#index_m"},
 {text:"o",url:"functions.html#index_o"},
@@ -58,7 +57,6 @@ var menudata={children:[
 {text:"Functions",url:"functions_func.html",children:[
 {text:"b",url:"functions_func.html#index_b"},
 {text:"c",url:"functions_func.html#index_c"},
-{text:"d",url:"functions_func.html#index_d"},
 {text:"e",url:"functions_func.html#index_e"},
 {text:"g",url:"functions_func.html#index_g"},
 {text:"i",url:"functions_func.html#index_i"},
